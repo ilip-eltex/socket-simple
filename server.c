a8137ad24af1f73e902de712350f3404e89ffd10
Igor Lipovtsev <ilip.z@ya.ru>
2020-05-14 15:00:11 +0700

    Add server part

--- server.c
+++ server.c
@@ -1,14 +1,35 @@
+// Server
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
 int main ()
 {
-	int toServer = socket(AF_INET, SOCK_STREAM, 0);
-	if (toServer == -1) 
+	int listenfd = 0, connfd = 0;
+	struct sockaddr_in serv_addr;
+	char sendBuff[32];
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons (7777);
+
+	bind(listenfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
+	listen(listenfd, 10);
+
+	while (1)
 	{
-		write(stderr, "Cann't get socket descr. App halted\n");
-		return 1;
-	}	
+        connfd = accept(listenfd, (struct sockaddr*) NULL, NULL);
+        strcpy(sendBuff, "You has been connected to server\n");
+        write(connfd, sendBuff, strlen(sendBuff));
+        close(connfd);
+        sleep(10);
+	}
+
+
 	return 0;
 }
