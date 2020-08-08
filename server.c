ab259e2ae76dafa62bd58a55c79c8d48fa384851
Igor Lipovtsev <ilip.z@ya.ru>
2020-08-08 13:40:25 +0700

    Make recv from client on serv

--- server.c
+++ server.c
@@ -9,25 +9,31 @@
 
 int main ()
 {
+	printf ("%d\n", sizeof(char));
 	int listenfd = 0, connfd = 0;
-	struct sockaddr_in serv_addr;
-	char sendBuff[1024];
-	memset(sendBuff, '0', sizeof(sendBuff));
+	struct sockaddr_in serv_addr, client_addr;
+	char sendBuff[1024], recvBuff[5];
+	memset(sendBuff, '\0', sizeof(sendBuff));
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv_addr.sin_addr.s_addr = inet_addr("192.168.0.5");
 	serv_addr.sin_port = htons (7777);
     printf("Wait for connection...\n");
 	bind(listenfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
 	listen(listenfd, 10);
 	connfd = accept(listenfd, (struct sockaddr*) NULL, NULL);
     printf("Accept connection\n");
-    strcpy(sendBuff, "Hello!\n");
-    printf ("sending: %s", sendBuff);
-        send(listenfd, (void*)sendBuff, sizeof("Hello!\n"), 0);
-        close(listenfd);
+    	while (1)
+	{
+		
+		recv (connfd, (void*) recvBuff, 5, 0);
+		printf ("%s", recvBuff);
+		memset (sendBuff, '\0', sizeof(sendBuff));
 
+	}
+        close(listenfd);
+	printf("a?\n");
 
 
 	return 0;
